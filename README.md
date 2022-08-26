7d5a92c8d43e278c3484248b27df25cb2371cdf5
Dave Hall <dave.hall@ibm.com>
2022-08-26 19:35:08 -0400

    Return arrays with diffs

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # evenfollowers
+
+See .env-sample for an example
+
+Set variables in .env
+
+## run
+```sh
+npm install
+npm run build
+npm start
+```
+
+https://pptr.dev/#debugging-tips
---
+++ main.ts
@@ -0,0 +1,86 @@
+import * as dotenv from "dotenv";
+import * as puppeteer from "puppeteer";
+
+declare function _getFollowers(): any;
+declare function _getFollows(): any;
+declare function _addFollower(athlete: string): any;
+declare function _addFollow(athlete: string): any;
+
+dotenv.config();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://strava.com/login", {
+    waitUntil: "networkidle0",
+  });
+
+  await page.waitForSelector("[name='email']");
+  await page.type("[name='email']", process.env.STRAVA_EMAIL as string);
+
+  await page.keyboard.down("Tab");
+  await page.keyboard.type(process.env.STRAVA_PASSWORD as string);
+
+  await page.keyboard.press("Enter");
+  await page.waitForSelector(".feed-header", { visible: true });
+
+  let followers = [] as string[];
+  let follows = [] as string[];
+  await page.exposeFunction("_getFollowers", function () {
+    return followers;
+  });
+  await page.exposeFunction("_getFollows", function () {
+    return follows;
+  });
+  await page.exposeFunction("_addFollower", function (athlete: string) {
+    return followers.push(athlete);
+  });
+
+  await page.exposeFunction("_addFollow", function (athlete: string) {
+    return follows.push(athlete);
+  });
+
+  const MAX_PAGES_FOLLOWERS_FOLLOWS_PAGE = parseInt(
+    process.env.STRAVA_ATHLETE_MAX_FOLLOWERS_FOLLOWS_PAGES as unknown as string
+  );
+  for (let i = 1; i < MAX_PAGES_FOLLOWERS_FOLLOWS_PAGE + 1; i++) {
+    await page.goto(
+      `https://www.strava.com/athletes/${process.env.STRAVA_ATHLETE_ID}/follows?page=${i}&page_uses_modern_javascript=true&type=following`,
+      {
+        waitUntil: "networkidle0",
+      }
+    );
+    await page.evaluate(() => {
+      document
+        .querySelectorAll("[data-athlete-id]")
+        .forEach(async function (element) {
+          await _addFollow(element.attributes[0].value);
+        });
+    });
+  }
+
+  for (let i = 1; i < MAX_PAGES_FOLLOWERS_FOLLOWS_PAGE + 1; i++) {
+    await page.goto(
+      `https://www.strava.com/athletes/${process.env.STRAVA_ATHLETE_ID}/follows?page=${i}&page_uses_modern_javascript=true&type=followers`,
+      {
+        waitUntil: "networkidle0",
+      }
+    );
+    await page.evaluate(() => {
+      document
+        .querySelectorAll("[data-athlete-id]")
+        .forEach(async function (element) {
+          await _addFollower(element.attributes[0].value);
+        });
+    });
+  }
+  console.log(
+    "I'M NOT FOLLOWING",
+    followers.filter((x) => !follows.includes(x))
+  );
+  console.log(
+    "NOT FOLLOWING ME",
+    follows.filter((x) => !followers.includes(x))
+  );
+  await browser.close();
+})();
